34a633190b4e4f176abf54f4c397d1192859a12d
felixz92 <contact@zippelf.com>
2021-05-01 18:50:11 +0200

    Deploy global stuff and crds via flux, not via script

--- hack/k3d/start.sh
+++ hack/k3d/start.sh
@@ -35,17 +35,21 @@ kubectl label node k3d-local-agent-1 storage=local --overwrite
 kubectl label node k3d-local-agent-2 storage=local --overwrite
 kubectl label node k3d-local-agent-2 node.kubernetes.io/ingress=traefik --overwrite
 
-export LOCALHOST_GATEWAY=$(docker network inspect k3d-local | jq -r '.[].IPAM.Config[].Gateway')
+export LOCALHOST_GATEWAY=$(kubectl -n kube-system get cm coredns -o jsonpath='{.data.NodeHosts}' | grep 'host.k3d.internal' | cut -d' ' -f1)
 
 envsubst '${LOCALHOST_GATEWAY}' < "${CURR_DIR}/coredns-patch.yaml" > "${CURR_DIR}/coredns-patch-substituted.yaml"
 
-kubectl -n kube-system patch cm coredns --patch "$(cat hack/k3d/coredns-patch-substituted.yaml)"
+test=$(kubectl get cm coredns -n kube-system --template='{{.data.NodeHosts}}' \
+  | sed -n -E -e '/[0-9\.]{4,12}\s+keycloak\.host\.k3d\.internal$/!p' -e "\$a${LOCALHOST_GATEWAY} keycloak.host.k3d.internal" \
+  | tr '\n' '^' \
+  | xargs -0 printf '{"data": {"NodeHosts":"%s"}}'\
+  | sed -E 's%\^%\\n%g') && kubectl patch cm coredns -n kube-system -p="$test"
 
 kubectl rollout restart deployment coredns -n kube-system
 
-deploy_global_resources "${BASEDIR}"
+#deploy_global_resources "${BASEDIR}"
 
-deploy_crds "${BASEDIR}"
+#deploy_crds "${BASEDIR}"
 
 #deploy_sealed_secrets "${BASEDIR}"
 
