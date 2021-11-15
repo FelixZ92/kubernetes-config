87c4102b1c315880940a6bd19723d162571cd350
Felix Zippel <contact@zippelf.com>
2021-11-15 06:02:43 +0100

    Stuff for hcloud

---
+++ hack/staging/start.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+set -x
+
+CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+[ -d "$CURR_DIR" ] || {
+  echo "FATAL: no current dir (maybe running in zsh?)"; exit 1
+}
+
+BASEDIR=$(dirname "$(dirname "$CURR_DIR")")
+
+export BASE_DOMAIN=static.24.192.12.49.clients.your-server.de
+export ENVIRONMENT=staging
+
+# shellcheck source=hack/secrets/secrets-common.sh
+source "$CURR_DIR/../secrets/common.sh"
+# shellcheck source=hack/common.sh
+source "$CURR_DIR/../common.sh"
+
+export KUBECONFIG="${HOME}/.kube/kubeconfig-rke2-staging.yaml"
+
+deploy_global_resources "${BASEDIR}"
+
+deploy_flux "${BASEDIR}" "$HOME/.ssh/gitlab_deploy_key" "$BASEDIR/hack/known_hosts" "${ENVIRONMENT}"
+
+kustomize build "$BASEDIR/01_gitops/bootstrap/overlays/${ENVIRONMENT}/" | envsubst | kubectl apply -f -
+
+kubectl wait --for=condition=ready --timeout=600s kustomizations.kustomize.toolkit.fluxcd.io -n flux-system bootstrap
+kubectl wait --for=condition=ready --timeout=600s kustomizations.kustomize.toolkit.fluxcd.io -n flux-system sealed-secrets
+apply_secrets "${BASEDIR}" "${ENVIRONMENT}"
+
+#
+#echo "Use with export KUBECONFIG=${KUBECONFIG}"
