f5784e53b3da23c04f0968fa8bb0757407d1ddd7
Felix Zippel <contact@zippelf.com>
2022-03-04 01:42:26 +0100

    Update secrets

--- hack/common.sh
+++ hack/common.sh
@@ -13,7 +13,7 @@ apply_secrets() {
   echo "Waiting for sealed secrets to become available"
   kubectl wait --for=condition=ready --timeout=600s kustomizations.kustomize.toolkit.fluxcd.io -n kube-system sealed-secrets
   set -x
-  gopass-kubeseal applyBulk -f "$BASEDIR/system/secrets/${ENVIRONMENT}/secrets.yaml"
+  gopass-kubeseal apply bulk -f "$BASEDIR/system/secrets/${ENVIRONMENT}/secrets.yaml"
 }
 
 deploy_flux() {
